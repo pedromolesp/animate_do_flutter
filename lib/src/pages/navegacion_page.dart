19ceb9b52f1bee43b7eedc32411ca53b79339cbd
pedromolesp <pedromolesp@gmail.com>
2021-04-20 16:55:44 +0200

    terminada la sección 9

--- lib/src/pages/navegacion_page.dart
+++ lib/src/pages/navegacion_page.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
@@ -23,39 +24,56 @@ class NavegacionPage extends StatelessWidget {
 class BottomNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final int numero = Provider.of<_NotificationModel>(context).numero;
-    return BottomNavigationBar(
-        currentIndex: 0,
-        selectedItemColor: Colors.pink,
-        items: [
-          BottomNavigationBarItem(
-              label: "Bones ", icon: FaIcon(FontAwesomeIcons.bone)),
-          BottomNavigationBarItem(
-            label: "Notifications ",
-            icon: Stack(
-              children: [
-                FaIcon(FontAwesomeIcons.bell),
-                Positioned(
-                  right: 0,
-                  top: 0,
-                  child: Container(
-                    width: 12,
-                    height: 12,
-                    decoration: BoxDecoration(
-                        color: Colors.redAccent, shape: BoxShape.circle),
-                    child: Text(
-                      '$numero',
-                      style: TextStyle(color: Colors.white, fontSize: 8),
-                    ),
-                    alignment: Alignment.center,
-                  ),
-                )
-              ],
-            ),
-          ),
-          BottomNavigationBarItem(
-              label: "My Dog ", icon: FaIcon(FontAwesomeIcons.dog))
-        ]);
+    final int numero =
+        Provider.of<_NotificationModel>(context, listen: false).numero;
+    return Builder(
+      builder: (context) {
+        return BottomNavigationBar(
+            currentIndex: 0,
+            selectedItemColor: Colors.pink,
+            items: [
+              BottomNavigationBarItem(
+                  label: "Bones ", icon: FaIcon(FontAwesomeIcons.bone)),
+              BottomNavigationBarItem(
+                label: "Notifications ",
+                icon: Stack(
+                  children: [
+                    FaIcon(FontAwesomeIcons.bell),
+                    Positioned(
+                      right: 0,
+                      top: 0,
+                      child: BounceInDown(
+                        from: 10,
+                        animate: numero > 0,
+                        child: Bounce(
+                          from: 10,
+                          controller: (controller) =>
+                              Provider.of<_NotificationModel>(context)
+                                  .bounceController = controller,
+                          child: Container(
+                            width: 12,
+                            height: 12,
+                            decoration: BoxDecoration(
+                                color: Colors.redAccent,
+                                shape: BoxShape.circle),
+                            child: Text(
+                              '$numero',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 8),
+                            ),
+                            alignment: Alignment.center,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              BottomNavigationBarItem(
+                  label: "My Dog ", icon: FaIcon(FontAwesomeIcons.dog))
+            ]);
+      },
+    );
   }
 }
 
@@ -68,20 +86,32 @@ class BotonFlotante extends StatelessWidget {
       ),
       backgroundColor: Colors.pink,
       onPressed: () {
-        int numero =
-            Provider.of<_NotificationModel>(context, listen: false).numero;
+        final notimodel =
+            Provider.of<_NotificationModel>(context, listen: false);
+        int numero = notimodel.numero;
         numero++;
-        Provider.of<_NotificationModel>(context, listen: false).numero = numero;
+        notimodel.numero = numero;
+        if (numero >= 2) {
+          final controller = notimodel.bounceController;
+          controller.forward(from: 0.0);
+        }
       },
     );
   }
 }
 
 class _NotificationModel extends ChangeNotifier {
+  AnimationController _bounceController;
   int _numero = 0;
   int get numero => this._numero;
   set numero(int valor) {
-    this.numero = valor;
+    this._numero = valor;
+    notifyListeners();
+  }
+
+  AnimationController get bounceController => this._bounceController;
+  set bounceController(AnimationController controller) {
+    this._bounceController = controller;
     notifyListeners();
   }
 }
